92eca87df55f5ed85baaf6787f7d9aac31e6f1e5
Antti Arppe <arppe@ualberta.ca>
2014-06-07 00:00:58 +0000

    Corrected script to work with multiple-spaced stems.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/hdn@95842 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- inc/generate-separate-yaml-files.sh
+++ inc/generate-separate-yaml-files.sh
@@ -2,10 +2,17 @@
 
 # generate-separate-yaml-files
 
-gawk 'BEGIN { class=""; verb=""; }
+gawk '{ if(index($0,"LEXICON")!=0)
+          { gsub("[ ]+","\t",$0); print; }
+        else
+          { gsub("[ ][ ]+","\t",$0); print; }
+}' |
+
+gawk 'BEGIN { FS="\t"; class=""; verb=""; }
 { if(index($0,"CLASS")!=0)
     { if(class!="") print "" >> fname;
-      class=$3; fname="/dev/stdout"; fname="V-"class"_gt-norm.yaml";
+      class=$3; # fname="/dev/stdout";
+      fname="V-"class"_gt-norm.yaml";
 
 print "Config:" > fname;
 print "  hfst:" >> fname;
@@ -19,14 +26,11 @@ print "" >> fname;
 print "Tests:" >> fname;
 print "" >> fname;
 getline; getline;
-verb=$NF; gsub("\+.*","",verb);
+verb=$2; gsub("\\+.*","",verb);
 print "  Verb - "verb" - ENGLISH : # "class >> fname;
 print "" >> fname
 }
 if(index($0,"CLASS")==0 && NF>0)
-{ printf "     %s:", $NF >> fname;
-  for(i=2; i<NF; i++)
-     printf " %s", $i >> fname;
-  printf "\n" >> fname;
+{ printf "     %s%s\n", $2, $1 >> fname;
 }}
 END { print "" >> fname; fflush(); }'
